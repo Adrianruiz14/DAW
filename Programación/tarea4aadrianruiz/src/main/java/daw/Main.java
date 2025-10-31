0b4e679751ba1c8c89369a54e36b016ee47abe8c
Adrian <aruiagu307@g.educaand.es>
2025-10-31 13:51:44 +0100

    memuero

--- Programación/tarea4aadrianruiz/src/main/java/daw/Main.java
+++ Programación/tarea4aadrianruiz/src/main/java/daw/Main.java
@@ -23,12 +23,12 @@ public class Main {
             JOptionPane.showMessageDialog(null, "Paco te señala la ventana");
             decision = "ventana";
 
-        } else  {
+        } else {
             JOptionPane.showMessageDialog(null, "Paco te señala la puerta");
             decision = "puerta";
         }
 
-        //Tu eliges si entrar o no, segun tu eleccion:
+        // Tu eliges si entrar o no, segun tu eleccion:
 
         do {
 
@@ -36,22 +36,24 @@ public class Main {
 
         } while (!(entrada.equalsIgnoreCase(SI) || entrada.equalsIgnoreCase(NO)));
 
-        switch (entrada) { //Si = entras
+        switch (entrada) { // Si = entras
             case "si":
 
-                if (decision == "ventana") JOptionPane.showMessageDialog(null, "Entras voluntariamente por la ventana");
-                if (decision == "puerta") JOptionPane.showMessageDialog(null, "Entras voluntariamente por la puerta");
+                if (decision == "ventana")
+                    JOptionPane.showMessageDialog(null, "Entras voluntariamente por la ventana");
+                if (decision == "puerta")
+                    JOptionPane.showMessageDialog(null, "Entras voluntariamente por la puerta");
 
                 break;
 
-                // No = paco te obliga a entrar da igual luego lo que digas
+            // No = paco te obliga a entrar da igual luego lo que digas
 
             case "no":
                 JOptionPane.showMessageDialog(null, "Paco te mira y te vuelve a preguntar");
 
                 for (int paco = 3; paco > 0; paco--) {
 
-                        entrada = JOptionPane.showInputDialog("¿Quieres entrar?");
+                    entrada = JOptionPane.showInputDialog("¿Quieres entrar?");
 
                     if (paco == 3) {
                         JOptionPane.showMessageDialog(null, "Paco se acerca");
@@ -63,17 +65,51 @@ public class Main {
                         JOptionPane.showMessageDialog(null,
                                 "Paco está detrás de tí, Paco te lanza por la ventana a la fuerza");
 
-                    if (decision == "puerta" && paco == 1) { JOptionPane.showMessageDialog(null,
-                                "Paco está detrás de tí, Paco te empuja por la puerta a la fuerza");
-                        
+                        if (decision == "puerta" && paco == 1) {
+                            JOptionPane.showMessageDialog(null,
+                                    "Paco está detrás de tí, Paco te empuja por la puerta a la fuerza");
+
+                        }
                     }
-                    }
-                    
+
                 }
 
         }
+        // Dentro de la casa;
 
+        switch (decision) {
+            case "puerta":
+                String eleccion1 = "";
+                String Salon = "Salon";
+                String Biblioteca = "Biblioteca";
+                JOptionPane.showMessageDialog(null,
+                        "Estas dentro la casa y ves 2 habitaciones, una biblioteca y un salon");
 
+                do {
+                    eleccion1 = JOptionPane.showInputDialog("¿Cual eliges? (Salon/Biblioteca)");
 
+                } while (!(eleccion1.equalsIgnoreCase(Salon) || eleccion1.equalsIgnoreCase(Biblioteca)));
+
+                break;
+
+            case "ventana":
+             String eleccion2 = "";
+                String Despensa = "Despensa";
+                String Pasillo = "Pasillo";
+                JOptionPane.showMessageDialog(null,
+                        "Estas dentro la casa y ves 2 habitaciones, una bdespensa y un pasillo");
+
+                do {
+                    eleccion1 = JOptionPane.showInputDialog("¿Cual eliges? (Despensa/Pasillo)");
+
+                } while (!(eleccion2.equalsIgnoreCase(Despensa) || eleccion2.equalsIgnoreCase(Pasillo)));
+
+            default:
+                break;
+        }
+
+      
+ 
     }
+
 }
